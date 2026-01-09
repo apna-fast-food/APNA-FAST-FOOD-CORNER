59d2a0b0dd0ead2699a614918f8386ca439e5a44
apna-fast-food <apnafastfood8@gmail.com>
2026-01-09 22:29:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,18 @@ let discount = "";
 
 function order(item) {
   let msg = "Hello, I want to order " + item;
-  if (discount !== "") msg += " | " + discount;
-  window.open("https://wa.me/919368871417?text=" + encodeURIComponent(msg));
+  if (discount !== "") {
+    msg += " | Offer: " + discount;
+  }
+  let url = "https://wa.me/919368871417?text=" + encodeURIComponent(msg);
+  window.open(url, "_blank");
 }
 
 function quiz(ans) {
   let res = document.getElementById("quizResult");
   if (ans === "correct") {
-    discount = "10% Discount Applied";
-    res.innerHTML = "🎉 Correct! You got 10% discount";
+    discount = "10% Discount";
+    res.innerHTML = "🎉 Correct! You won 10% OFF";
   } else {
     res.innerHTML = "❌ Wrong! Try again";
   }
@@ -18,8 +21,7 @@ function quiz(ans) {
 
 function spin() {
   let offers = ["5% OFF", "10% OFF", "Free Momos", "Better Luck Next Time"];
-  let pick = offers[Math.floor(Math.random()*offers.length)];
+  let pick = offers[Math.floor(Math.random() * offers.length)];
   document.getElementById("spinResult").innerHTML = "🎁 " + pick;
   discount = pick;
 }
-
